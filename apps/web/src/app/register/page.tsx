4a083e03778c923f50859a58fb73ce307fde426a
rezzadtyp <rezzadityap@gmail.com>
2024-04-30 15:12:32 +0700

    feat: add register

---
+++ apps/web/src/app/register/page.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import FormInput from '@/components/FormInput';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { useFormik } from 'formik';
+import { validationSchema } from './validationSchema';
+import useRegister from '@/hooks/api/auth/useRegister';
+
+const Register: React.FC = () => {
+  const { register } = useRegister();
+  const { values, touched, errors, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        name: '',
+        email: '',
+        password: '',
+      },
+      validationSchema,
+      onSubmit: (values) => {
+        register(values);
+      },
+    });
+
+  return (
+    <main className="container mx-auto px-4">
+      <div className="mt-20 flex justify-center">
+        <Card className="w-[450px]">
+          <CardHeader>
+            <CardTitle className="text-center text-3xl text-primary">
+              Register
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="grid w-full items-center gap-4">
+                {/* NAME */}
+                <FormInput
+                  name="name"
+                  error={errors.name}
+                  isError={!!touched.name && !!errors.name}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="Name"
+                  type="text"
+                  value={values.name}
+                />
+
+                {/* EMAIL */}
+                <FormInput
+                  name="email"
+                  error={errors.email}
+                  isError={!!touched.email && !!errors.email}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="Email"
+                  type="email"
+                  value={values.email}
+                />
+
+                {/* PASSWORD */}
+                <FormInput
+                  name="password"
+                  error={errors.password}
+                  isError={!!touched.password && !!errors.password}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder="Password"
+                  type="password"
+                  value={values.password}
+                />
+              </div>
+              <Button className="mt-6 w-full">Register</Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </main>
+  );
+};
+
+export default Register;

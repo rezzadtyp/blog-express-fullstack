d6eca609307ee8ec367b472ec21a83b11669e490
rezzadtyp <rezzadityap@gmail.com>
2024-05-02 15:35:56 +0700

    feat: forgot-password

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 import { registerService } from '@/services/auth/register.service';
 import { loginService } from '@/services/auth/login.service';
+import { forgotPasswordService } from '@/services/auth/forgot-password.service';
 
 export class AuthController {
   async registerController(req: Request, res: Response, next: NextFunction) {
     try {
       const result = await registerService(req.body);
-
       res.status(200).send(result);
     } catch (error) {
       next(error);
@@ -16,7 +16,19 @@ export class AuthController {
   async loginController(req: Request, res: Response, next: NextFunction) {
     try {
       const result = await loginService(req.body);
+      res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
+  }
 
+  async forgotPasswordController(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const result = await forgotPasswordService(req.body.email);
       res.status(200).send(result);
     } catch (error) {
       next(error);
